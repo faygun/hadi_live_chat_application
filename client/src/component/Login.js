d7a1a6e922826181e4138ea2fce5d8084a65efd0
DESKTOP-5GLTOPK\faygu <faygun89@gmail.com>
2019-08-23 15:58:37 +0300

    developed button submit func.

--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -1,6 +1,8 @@
 import React,{Component} from 'react';
-import { getSpinner } from '../helper/helper';
+import axios from 'axios';
 import '../login.css';
+import { isAuthenticated } from '../helper/jwt';
+import { getSpinner } from '../helper/helper';
 
 export default class Login extends Component{
     constructor(prop){
@@ -14,9 +16,47 @@ export default class Login extends Component{
         });
         
     }
+    
+    componentDidMount(){
+        if(isAuthenticated())
+            this.props.history.push('/channels');
+    }
+
     submitLogin(e) {
+        this.setState({error:''});
+
         e.preventDefault();
-        return;
+        if(!this.validateForm()){
+            this.setState({error:'Please insert email or password'});
+            return;
+        }
+        
+        this.setState({loadingType:'cubes'});
+
+        const config  = {
+            headers:{
+                'Content-Type' : 'application/json'
+            }
+        }
+        
+        var request = {email : this.state.email, password : this.state.password};
+        const body = JSON.stringify(request);
+        
+        axios.post('/api/auth', body, config)
+        .then(res => {
+            this.setState({loadingType:''});
+            localStorage.setItem("x-auth-token", res.data.token);
+            localStorage.setItem("user", JSON.stringify(res.data));
+            this.props.history.push('/channels');
+        })
+        .catch(err => {
+            this.setState({loadingType:'', error:err.response.data});
+        });
+    }
+
+    //Basic validation.
+    validateForm() {
+        return this.state.email.length > 0 && this.state.password.length > 0;
     }
 
     render(){
