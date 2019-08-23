3357ce1355a3a06be6a7b64d9be9802ff5734b13
DESKTOP-5GLTOPK\faygu <faygun89@gmail.com>
2019-08-23 23:59:00 +0300

    implented channels api in the component

--- client/src/component/Channel.js
+++ client/src/component/Channel.js
@@ -3,15 +3,41 @@ import { Container, Row, Col } from 'reactstrap';
 import '../Channel.css';
 import ChannelBar from './ChannelBar';
 import MessageContainer from './MessageContainer';
+import axios from 'axios';
+import { getConfig } from '../helper/jwt';
 
 export default class Channel extends Component{
+    constructor(props){
+        super(props)
 
+        this.state = {
+            channels:[],
+            active_channel : ""
+        };
+    }
+
+    setActiveChannel(id){
+        this.setState({
+            active_channel : id
+        })
+    }
+
+    componentDidMount(){
+        axios.get('/api/channel', getConfig())
+        .then(res => {
+            this.setState({channels:res.data})
+        })
+        .catch(err => {
+            this.setState({error:err.error});
+        });
+    }
     render(){
+        let channelList = this.state.channels ? this.state.channels : [];
         return(
             <Container>
                 <Row>
-                    <Col xs="3"><ChannelBar/></Col>
-                    <Col xs="9"><MessageContainer/></Col>
+                    <Col xs="3"><ChannelBar setActiveChannel={this.setActiveChannel.bind(this)} channels={channelList}/></Col>
+                    <Col xs="9"><MessageContainer active_channel={this.state.active_channel}/></Col>
                 </Row>
             </Container>
         )
--- client/src/component/ChannelBar.js
+++ client/src/component/ChannelBar.js
@@ -1,37 +1,23 @@
 import React,{Component} from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
-import axios from 'axios';
-import { getConfig } from '../helper/jwt';
 
 export default class ChannelBar extends Component{
     constructor(props){
-        super(props)
+        super(props);
 
         this.state = {
-            channels:[],
-            error:''
-        };
-    }
-    
-    componentDidMount(){
-        axios.get('/api/channel', getConfig())
-        .then(res => {
-            this.setState({channels:res.data})
-        })
-        .catch(err => {
-            this.setState({error:err.error});
-        });
+            active_id :""
+        }
     }
     render(){
-        let channelList = this.state.channels ? this.state.channels : [];
         return(
             <div className="channel-bar">
                 <p>Channels</p>
                 <Nav vertical color="dark" width="25%">
                     {
-                        channelList.map(({id,name}) => (  
+                        this.props.channels.map(({id,name}) => (  
                             <NavItem>
-                                    <NavLink onClick={} channel-id={id} href="#">{name}</NavLink>
+                                    <NavLink key={id} className={this.state.active_id === id ? "active" : ""} onClick={()=>{this.setState({active_id : id}); this.props.setActiveChannel(id)}} channel-id={id} href="#">{name}</NavLink>
                             </NavItem>  
                         ))
                     }
