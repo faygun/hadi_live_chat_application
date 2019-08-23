1cf18db6b944fea5b1a807b1e60672ea1087d265
DESKTOP-5GLTOPK\faygu <faygun89@gmail.com>
2019-08-23 16:01:57 +0300

    developed getConfig helper func.

--- client/src/helper/helper.js
+++ client/src/helper/helper.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactLoading from 'react-loading';
+import { getJwt } from "./jwt";
 
 export const getSpinner = (width, height) =>{
     return (<ReactLoading type="cubes" width={width} height={height} className="spinner" color="#39ace7"  />);
+}
+
+export const getConfig = ()=> {
+    const config = {
+        headers:{
+            "Content-type":"application/json"
+        }
+      };
+      const jwt = getJwt();
+      config.headers['x-auth-token'] = jwt;
+
+      return config;
 }
